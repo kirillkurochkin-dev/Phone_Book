37d87ab5c43c2f9559a464b9a52e0ac7ef290cb8
Kirikura <kirikura.w@yandex.ru>
2024-02-11 20:38:03 +0300

    Added random data generation

--- phoneBook.go
+++ phoneBook.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"path"
+	"strconv"
 )
 
 type person struct {
@@ -14,9 +16,9 @@ type person struct {
 
 var phoneBook []person
 
-func search(surname string) *person {
+func search(phone string) *person {
 	for _, pers := range phoneBook {
-		if pers.surname == surname {
+		if pers.phone == phone {
 			return &pers
 		}
 	}
@@ -29,6 +31,29 @@ func list() {
 	}
 }
 
+func getString(len int) string {
+	temp := ""
+	startChar := "!"
+
+	for i := 0; i < len; i++ {
+		myRand := rand.Intn(26) + 65
+		newChar := string(startChar[0] + byte(myRand))
+		temp = temp + newChar
+	}
+
+	return temp
+}
+
+func populate(n int) {
+	for i := 0; i < n; i++ {
+		phoneBook = append(phoneBook, person{
+			name:    getString(rand.Intn(10)),
+			surname: getString(rand.Intn(10)),
+			phone:   strconv.Itoa(rand.Intn(899999999) + 100000000),
+		})
+	}
+}
+
 func main() {
 	args := os.Args
 	if len(args) == 1 {
@@ -36,7 +61,9 @@ func main() {
 		fmt.Printf("Usage: %s search|list <arguments>\n", exe)
 	}
 
-	phoneBook = append(phoneBook, person{
+	populate(10)
+
+	/*phoneBook = append(phoneBook, person{
 		name:    "Alex",
 		surname: "Fern",
 		phone:   "324809723",
@@ -45,12 +72,12 @@ func main() {
 		name:    "Jean",
 		surname: "Morningstar",
 		phone:   "234809384",
-	})
+	})*/
 
 	switch args[1] {
 	case "search":
 		if len(args) != 3 {
-			fmt.Printf("Usage: search Username")
+			fmt.Printf("Usage: search Phone")
 			return
 		}
 		pers := search(args[2])
